dbe1b0e3e0a9d7e5f54e247a2c3f4ba0953b796c
Masashi Kondo <45549311+MssKnd@users.noreply.github.com>
2022-05-26 09:07:53 +0900

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import { useState } from "react";
+import "./styles.css";
+
+export default function App() {
+  const [value, setValue] = useState<string | null>(null);
+  const handleInput = (changeEvent: React.ChangeEvent<HTMLInputElement>) => {
+    const file = changeEvent.target.files ? changeEvent.target.files[0] : null;
+    if (!file) {
+      return;
+    }
+    const reader = new FileReader();
+    reader.onload = (event: ProgressEvent<FileReader>) => {
+      if (typeof event?.target?.result === "string") {
+        setValue(event.target.result);
+      }
+    };
+    reader.readAsDataURL(file);
+  };
+
+  return (
+    <div className="App">
+      <input type="file" accept="image/*" onChange={handleInput} />
+      {value && <img src={value} alt={"preview"} />}
+    </div>
+  );
+}
